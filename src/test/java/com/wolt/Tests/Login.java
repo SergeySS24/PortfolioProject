faf3cfce9a57a6e97d645d431719fec6675e706e
Sergey Starostin <adelaide.star@gmail.com>
2022-11-09 09:33:17 +0300

    Added login test

---
+++ src/test/java/com/wolt/Tests/Login.java
@@ -0,0 +1,30 @@
+package com.wolt.Tests;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Login {
+
+    @BeforeAll
+    static void preparation() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+    @Test
+    void login() {
+        open("https://wolt.com/en/cyp");
+        $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
+        $(".Button-module__button___WpJP1").click();
+        $(".CitySelection-module__container___xXS1B").scrollTo().shouldHave(Condition.text("Limassol"));
+        $(".List-module__listOfCities____EgRr").$(".ListItem-module__container___ji0Cg", 1).click();
+    }
+
+}
---
+++ src/test/java/com/wolt/TestsSupport/LoginSupport.java
@@ -0,0 +1,4 @@
+package com.wolt.TestsSupport;
+
+public class LoginSupport {
+}
