8d30703b63aee3c8fb3ce033f8d447f73a454b10
Sergey Starostin <adelaide.star@gmail.com>
2022-11-13 01:14:34 +0300

    Added main page logo tests

--- src/test/java/com/wolt/Tests/Login.java
+++ src/test/java/com/wolt/Tests/Login.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class Login {
 
@@ -23,9 +24,53 @@ public class Login {
         open("https://wolt.com/en/cyp");
         $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
         $(".Button-module__button___WpJP1").click();
+    }
+
+    @Test
+    void logoCheck() {
+        open("https://wolt.com/en/cyp");
+        $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
+        $(".Button-module__button___WpJP1").click();
+
+        $(".MainHeader__LogoLink-sc-5qxwnz-2").shouldHave(Condition.href("/en/discovery"));
+    }
+
+    @Test
+    void logoClick() {
+        open("https://wolt.com/en/cyp");
+        $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
+        $(".Button-module__button___WpJP1").click();
+
+        $(".MainHeader__LogoLink-sc-5qxwnz-2").click();
+        webdriver().shouldHave(url("https://wolt.com/en/discovery"));
+    }
+
+
+
+
+    @Test
+    void switchToLimassol() {
+        open("https://wolt.com/en/cyp");
+        $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
+        $(".Button-module__button___WpJP1").click();
         $(".CitySelection-module__container___xXS1B").scrollTo().shouldHave(Condition.text("Limassol"));
         $(".List-module__listOfCities____EgRr").$(".ListItem-module__container___ji0Cg", 2).click();
         $(".ControlledDeliveryLocationButton__LocationName-sc-124bug2-2").shouldHave(Condition.text("Limassol"));
     }
+
+    @Test
+    void locationChangePopup() {
+
+        open("https://wolt.com/en/cyp");
+        $(".ConsentsBanner__Root-sc-194d5oz-0").$(".Button__Root-sc-a3fg5q-2").click();
+        $(".Button-module__button___WpJP1").click();
+        $(".CitySelection-module__container___xXS1B").scrollTo().shouldHave(Condition.text("Limassol"));
+        $(".List-module__listOfCities____EgRr").$(".ListItem-module__container___ji0Cg", 2).click();
+        $(".ControlledDeliveryLocationButton__LocationName-sc-124bug2-2").shouldHave(Condition.text("Limassol"));
+
+        $(".Popover__Root-sc-mo27do-0")
+                .shouldHave(Condition.text("You can change your delivery location here. Happy Wolting \uD83D\uDC99"));
+    }
+
 
 }
