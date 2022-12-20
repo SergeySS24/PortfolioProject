6a50da1d335c91fe80684854065e719773051d42
Sergey Starostin <adelaide.star@gmail.com>
2022-12-20 19:16:47 +0300

    Added continued RestaurantPageCheck

--- src/test/java/com/wolt/Tests/RestaurantPageCheck.java
+++ src/test/java/com/wolt/Tests/RestaurantPageCheck.java
@@ -4,8 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.wolt.TestsSupport.Base3;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RestaurantPageCheck extends Base3 {
 
@@ -99,10 +98,50 @@ public class RestaurantPageCheck extends Base3 {
         $$(".MenuCategoryHeader__Heading-sc-1enduc0-0").find(Condition.text("DESSERTS")).shouldHave(Condition.text("DESSERTS")).shouldBe(Condition.visible);
         $$(".MenuItem-module__itemContainer____1T8k").find(Condition.text("Milk Chocolate Cookie"))
                 .shouldHave(Condition.text("Milk Chocolate Cookie")).shouldHave(Condition.text("€4.00")).shouldBe(Condition.visible);
+        $$(".MenuItem-module__toggle___iTf9H")
+                .find(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/milk-chocolate-cookie-itemid-638f18a4512bb58766b10e83"))
+                .shouldHave(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/milk-chocolate-cookie-itemid-638f18a4512bb58766b10e83"));
         $$(".MenuItem-module__itemContainer____1T8k").find(Condition.text("Pistachio Cookie"))
                 .shouldHave(Condition.text("Pistachio Cookie")).shouldHave(Condition.text("€4.50")).shouldBe(Condition.visible);
+        $$(".MenuItem-module__toggle___iTf9H")
+                .find(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/pistachio-cookie-itemid-638f18b4d454e9f4bdbc7d94"))
+                .shouldHave(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/pistachio-cookie-itemid-638f18b4d454e9f4bdbc7d94"));
         $$(".MenuItem-module__itemContainer____1T8k").find(Condition.text("Halva Brownie"))
                 .shouldHave(Condition.text("Halva Brownie")).shouldHave(Condition.text("€4.50")).shouldBe(Condition.visible);
+        $$(".MenuItem-module__toggle___iTf9H")
+                .find(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/halva-brownie-itemid-638f18c0d786bf1cf72f49c7"))
+                .shouldHave(Condition.href("/en/cyp/limassol/restaurant/the-smuggers/halva-brownie-itemid-638f18c0d786bf1cf72f49c7"));
     }
 
+
+    @Test
+    void productOpenCheck() {
+        $$(".Categories-module__categoryLink___WM_5v").find(Condition.text("DESSERTS")).shouldHave(Condition.text("DESSERTS")).click();
+        $$(".MenuCategoryHeader__Heading-sc-1enduc0-0").find(Condition.text("DESSERTS")).shouldHave(Condition.text("DESSERTS")).shouldBe(Condition.visible);
+        $$(".MenuItem-module__itemContainer____1T8k").find(Condition.text("Milk Chocolate Cookie"))
+                .shouldHave(Condition.text("Milk Chocolate Cookie")).shouldHave(Condition.text("€4.00")).click();
+        $(".ProductViewScreen__PaddedItem-sc-1pnst4x-1").shouldHave(Condition.text("Milk Chocolate Cookie"));
+        $(".UnitAndPriceInfo__OriginalPrice-sc-1t66qe7-7").shouldHave(Condition.text("€4.00"));
+        $(".ModalNavigationButton__Content-sc-1x1lz0u-1").shouldHave(Condition.text("Product info"));
+        $$(".Stepper__VisibleInput-sc-ho8tch-1").find(Condition.text("1")).shouldHave(Condition.text("1"));
+        $$(".ProductViewFooter__SubmitButtonContent-sc-34t03j-5").find(Condition.text("Add to order")).shouldHave(Condition.text("Add to order"));
+        $$(".ProductViewFooter__Price-sc-34t03j-6").find(Condition.text("€4.00")).shouldHave(Condition.text("€4.00"));
+
+        $(".ModalNavigationButton__Content-sc-1x1lz0u-1").shouldHave(Condition.text("Product info")).click();
+
+
+
+        //$$(".Heading_root__IDsAG").find(Condition.text("Allergens")).shouldHave(Condition.text("Allergens"));
+
+       // $(".InfoItem_description__WoARL").shouldHave(Condition.text("gluten, eggs, milk"));
+        $$(".ModalCardPage__ScrollContainer-sc-79y6nv-4").find(Condition.text("gluten, eggs, milk"))
+                .shouldHave(Condition.text("gluten, eggs, milk"));
+               // .shouldHave(Condition.text("Allergens")).shouldHave(Condition.text("gluten, eggs, milk"));
+
+
+
+
+
+
+    }
 }
